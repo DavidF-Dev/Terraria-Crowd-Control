51c46aeb2fd2695a5e8fb9e0a87234db309ae1f0
David F <account@davidfdev.com>
2022-06-24 17:29:16 +1000

    Added hook for when an item is crafted

---
+++ src/Globals/CrowdControlItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using JetBrains.Annotations;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CrowdControlMod.Globals;
+
+[UsedImplicitly]
+public sealed class CrowdControlItem : GlobalItem
+{
+    #region Events
+
+    /// <summary>
+    ///     Invoked when an item is crafted by the local player (client-side).
+    /// </summary>
+    [PublicAPI]
+    public static event Action<Recipe> OnCraftedHook;
+
+    #endregion
+
+    #region Methods
+
+    public override void OnCreate(Item item, ItemCreationContext context)
+    {
+        if (context is RecipeCreationContext recipeCreationContext)
+        {
+            OnCraftedHook?.Invoke(recipeCreationContext.recipe);
+        }
+    }
+
+    #endregion
+}
