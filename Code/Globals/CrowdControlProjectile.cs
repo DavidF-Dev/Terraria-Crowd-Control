0e84318020b743a9f41df2704f1a54e4857b48de
David F <account@davidfdev.com>
2024-05-04 09:28:06 +1000

    Replaced obsolete Kill with OnKill.

--- Code/Globals/CrowdControlProjectile.cs
+++ Code/Globals/CrowdControlProjectile.cs
@@ -8,21 +8,21 @@ public sealed class CrowdControlProjectile : GlobalProjectile
 {
     #region Delegates
 
-    /// <inheritdoc cref="Kill" />
+    /// <inheritdoc cref="OnKill" />
     public delegate void KillDelegate(Projectile projectile, int timeLeft);
 
     #endregion
 
     #region Events
 
-    /// <inheritdoc cref="Kill" />
+    /// <inheritdoc cref="OnKill" />
     public static event KillDelegate? KillHook;
 
     #endregion
 
     #region Methods
 
-    public override void Kill(Projectile projectile, int timeLeft)
+    public override void OnKill(Projectile projectile, int timeLeft)
     {
         KillHook?.Invoke(projectile, timeLeft);
     }
