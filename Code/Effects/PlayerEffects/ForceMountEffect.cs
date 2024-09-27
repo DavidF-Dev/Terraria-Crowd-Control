29bb2b3871b5287225e95dbca42ffd85ada45a05
David F <account@davidfdev.com>
2024-09-28 09:03:46 +1000

    Removed mech minecart from "Force Mount" effect.

--- Code/Effects/PlayerEffects/ForceMountEffect.cs
+++ Code/Effects/PlayerEffects/ForceMountEffect.cs
@@ -22,7 +22,7 @@ public sealed class ForceMountEffect : CrowdControlEffect
         BuffID.Flamingo, BuffID.DarkHorseMount, BuffID.MajesticHorseMount, BuffID.PaintedHorseMount, BuffID.LavaSharkMount,
         BuffID.BasiliskMount, BuffID.UnicornMount, BuffID.PigronMount, BuffID.QueenSlimeMount, BuffID.Rudolph, BuffID.ScutlixMount,
         BuffID.UFOMount, BuffID.WitchBroom, BuffID.CuteFishronMount, BuffID.DrillMount, BuffID.DarkMageBookMount, BuffID.WallOfFleshGoatMount,
-        BuffID.PirateShipMount, BuffID.SpookyWoodMount, BuffID.SantankMount, BuffID.MinecartRightMech, BuffID.MeowmereMinecartRight,
+        BuffID.PirateShipMount, BuffID.SpookyWoodMount, BuffID.SantankMount, BuffID.MeowmereMinecartRight,
         BuffID.WolfMount
     };
 
